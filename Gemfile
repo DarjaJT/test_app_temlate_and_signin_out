1e2cd053aa1509257cebcd1105c5191d0e3edd95
DarjaJestin <jestin.darja@gmail.com>
2017-09-04 09:44:12 +0300

    end registration

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
+gem 'bcrypt-ruby', '3.1.2'
 
 gem 'bootstrap-sass', '2.3.2.0'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,29 @@ class UsersController < ApplicationController
 
 
   def new # add new user
+    @user = User.new
+  end
 
+  def show # show user profile
+    @user = User.find(params[:id])
+  end
+
+  def create # registration form processing
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the App!" # temporary messages (application.html.erb)
+      redirect_to @user
+    else # failure to register
+      render 'new'
+    end
+  end
+
+
+
+  private
+
+  def user_params # parameters are required
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 
